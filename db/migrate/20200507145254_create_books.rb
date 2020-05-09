f3174a73bc1211e900570c15348a1c635971f7d3
Fuji-tori <out.fuji96aya@gmail.com>
2020-05-09 23:43:44 +0900

    book-migrate

--- db/migrate/20200507145254_create_books.rb
+++ db/migrate/20200507145254_create_books.rb
@@ -1,7 +1,13 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-
+      t.string :bookname, null: false
+      t.string :author, null: false
+      t.integer :price, null: false
+      t.string :text, null: false
+      t.text :image, null: false
+      t.text :review, null: false
+      t.string :genre, null: false, foreign_key: true
       t.timestamps
     end
   end
