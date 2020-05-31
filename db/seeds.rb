3dcd53fb72e1d03201368499fc3fe85e6782eafe
Fuji-tori <out.fuji96aya@gmail.com>
2020-05-31 23:39:01 +0900

    seeds-initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# 書籍
+books = BookGenre.create(name: "書籍")
+books_1 = .children.create(name: "")
+books_1.children.create([{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""},{name: ""}])
+
+# 雑誌
+magazine = BookGenre.create(name: "雑誌")
+magazine_1 = .children.create(name: "")
+
+# コミック
+comic = BookGenre.create(name: "コミック")
+comic_1 = .children.create(name: "")
+
+# ラノベ
+lightnovel = BookGenre.create(name: "ライトノベル")
+lightnovel_1 = .children.create(name: "")
+
+# 子供の本
+childbook = BookGenre.create(name: "こどもの本")
+childbook_1 = .children.create(name: "")
+
+# 電子書籍・コンテンツ
+ebook = BookGenre.create(name: "電子書籍・コンテンツ")
+ebook_1 = .children.create(name: "")
