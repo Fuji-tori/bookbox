55f73a6229a23e60e2908d02b62acca92d9e89ec
Fuji-tori <out.fuji96aya@gmail.com>
2020-06-02 23:55:12 +0900

    model-routes_fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :user
+  belongs_to :book_genre, optional: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
-  resources :books, only: [:index, :new]
+  resources :books, only: [:index, :new] do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
 end
