436494f1327b6498d3d2130627593637ff097884
Fuji-tori <out.fuji96aya@gmail.com>
2020-08-20 23:48:33 +0900

    user-show_each

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     redirect_to root_path unless user_signed_in?
     @nickname = current_user.nickname
+    @books = current_user.books
   end
 
   def edit
