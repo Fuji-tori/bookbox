538fe07a37cd6a887a8d94b34e8aab5ab4bbbdef
Fuji-tori <out.fuji96aya@gmail.com>
2020-09-17 21:58:39 +0900

    user-validates

--- app/controllers/Users/registrations_controller.rb
+++ app/controllers/Users/registrations_controller.rb
@@ -38,16 +38,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar, :header])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar, :header, :profile])
   end
 
   # The path used after sign up.
