20e04747ea7475aac730fd4b91d2387651a1a74f
Fuji-tori <out.fuji96aya@gmail.com>
2020-09-19 23:29:13 +0900

    delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,20 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, :true
-  validates :password, presence: true, length: { minimum: 7},
-                format: {
-                  with: /\A[a-z0-9]+\z/i,
-                  message: "は半角英数字で入力して下さい"
-                }
-  validates :email, presence: true,
-                format: {
-                  with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-                  message: "は正しいメールアドレスを入力して下さい"
-                }
-  
-  mount_uploader :avatar, AvatarUploader
          
   has_many :books
   has_many :comments
