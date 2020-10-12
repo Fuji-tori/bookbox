4ebad6abdd5d59f37c0def9cc56a13ffe2da2ae2
Fuji-tori <out.fuji96aya@gmail.com>
2020-10-12 23:09:45 +0900

    commentout-validates

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,13 @@ class Book < ApplicationRecord
   has_many :comments
 
   mount_uploader :image, ImageUploader
+
+  # validates :image, :book_genre_id, presence: {message: "を選択してください"}
+
+  # validates :book_name, :author, :publication, presence: true, length: { maximum: 100 }
+
+  # validates :synopsis, :review, presence: true,length: { maximum: 1000 }
+
+  # validates :price, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 10000000}
+
 end
