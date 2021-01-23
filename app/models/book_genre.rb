919338a6cf30386dacbe38e0c3f1aa6f75abf4e5
Fuji-tori <out.fuji96aya@gmail.com>
2021-01-23 22:35:56 +0900

    fix

--- app/models/book_genre.rb
+++ app/models/book_genre.rb
@@ -2,5 +2,5 @@ class BookGenre < ApplicationRecord
 
   has_many :books
   has_ancestry
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,5 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :comments
-  
+
 end
