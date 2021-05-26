f8d32e555419851947ddf798e442dadb8d62d313
Fuji-tori <out.fuji96aya@gmail.com>
2021-05-26 23:42:27 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
   get 'users/show'
   root to: 'books#index'
-
+  
   resources :books do
-
+    
     collection do
       get 'get_genre_children', defaults: { format: 'json' }
       get 'get_genre_grandchildren', defaults: { format: 'json' }
     end
-
+    
     get :search, on: :collection
     resources :comments, only: :create
   end
-
+  
   resources :users, only: [:show, :edit]
 
 end
