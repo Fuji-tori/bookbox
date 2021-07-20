885b85edd3efd6f6a25a3905b59281e4d082e530
Fuji-tori <out.fuji96aya@gmail.com>
2021-07-20 22:58:42 +0900

    js

--- app/assets/javascripts/time_box.js
+++ app/assets/javascripts/time_box.js
@@ -1,4 +1,6 @@
 window.onload = function() {
+
   let spinner = document.getElementById('my-spinner'); 
   spinner.classList.add('loaded');
+  
 }
--- app/assets/javascripts/user_list.js
+++ app/assets/javascripts/user_list.js
@@ -1,6 +1,8 @@
 $(function(){
+
   $("ul.mypage-left li").click(function(){
     $("ul.mypage-left li.selected").removeClass("selected");
     $(this).addClass("selected");
   });
+  
 });
