ffc6d22e98e73bd71bb7b83ec1ab612e02d74244
Fuji-tori <out.fuji96aya@gmail.com>
2021-09-10 22:42:35 +0900

    js-japanese

--- app/assets/javascripts/userhistory.js
+++ app/assets/javascripts/userhistory.js
@@ -7,14 +7,14 @@
     // [Product.name]で商品名称を取得
     book = "#{{@product.name}}";
     
-    // [Cookie]から[product_names]に関するデータを取得します。
+    // [Cookie]から[product_names]に関するデータを取得
     books = Cookies.get("product_names");
     
-    // [undefined]でなければデータが[product_name]に入っている。
+    // [undefined]でなければデータが[product_name]に入っている
     if (books != undefined) {
       books = books + "," + book;
       split = books.split(",");
-      // 重複を除去します。
+      // 重複を除去
       dup = split.filter(function(a, c, b) {
         return b.indexOf(a) === c;
       });
