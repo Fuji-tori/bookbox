03ab75c8c4605ba0587dc56a1c0df5f3486d7b4d
Fuji-tori <out.fuji96aya@gmail.com>
2024-06-08 23:29:43 +0900

    fix

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
-
+  
   before_action :set_book, except: [:index, :new, :create, :search, :get_genre_children, :get_genre_grandchildren]
-
+  
   def index
     @books = Book.all
   end
-
+  
   def new
     @book = Book.new
     @book_genre = BookGenre.where(ancestry: nil)
@@ -14,11 +14,11 @@ class BooksController < ApplicationController
   def get_genre_children
     @genre_children = BookGenre.find("#{params[:parent_name]}").children
   end
-
+  
   def get_genre_grandchildren
     @genre_grandchildren = BookGenre.find("#{params[:child_id]}").children
   end
-
+  
   def create
     @book = Book.new(book_params)
     @book_genre = BookGenre.where(ancestry: nil)
@@ -28,18 +28,18 @@ class BooksController < ApplicationController
       render :new
     end
   end
-
+  
   def show
     @comment = Comment.new
     @comments = @book.comments.includes(:user)
     # @recently_visit = cookies[:book_name].split(',') if cookies[:book_name]
   end
-
+  
   def edit
     @book = Book.find(params[:id])
     @book_genre = BookGenre.where(ancestry: nil)
   end
-
+  
   def search
     # if params[:book_name].present?
     @books = Book.where('book_name LIKE ?', "%#{params[:book_name]}%")
@@ -52,7 +52,7 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:book_name, :author, :publication, :price, :image, :synopsis, :review, :book_genre_id).merge(user_id: current_user.id)
   end
-
+  
   def set_book
     @book = Book.find(params[:id])
   end
