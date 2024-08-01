f48e5c69239fba9fc14d29bb3e878414f75dbd29
Fuji-tori <out.fuji96aya@gmail.com>
2024-08-01 19:01:13 +0900

    3

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  
   def create
     @comment = Comment.create(comment_params)
     respond_to do |format|
@@ -7,10 +7,10 @@ class CommentsController < ApplicationController
       format.json
     end
   end
-
+  
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, book_id: params[:book_id])
   end
-
+  
 end
