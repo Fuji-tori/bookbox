e4e585c9127b0281a28e4e4d8dabbbf9c3ff8718
Fuji-tori <out.fuji96aya@gmail.com>
2024-08-06 22:09:14 +0900

    8

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,14 @@
 class Book < ApplicationRecord
-
+  
   belongs_to :user
   belongs_to :book_genre, optional: true
   has_many :comments
-
+  
   mount_uploader :image, ImageUploader
-
+  
   # validates :image, :book_genre_id, presence: {message: "を選択してください"}
   # validates :book_name, :author, :publication, presence: true, length: { maximum: 100 }
   # validates :synopsis, :review, presence: true,length: { maximum: 1000 }
   # validates :price, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 10000000}
-
+  
 end
