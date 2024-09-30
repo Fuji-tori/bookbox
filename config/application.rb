61efa87e7060a86746ea4c9db92e5340c4a758a0
Fuji-tori <out.fuji96aya@gmail.com>
2024-09-30 23:30:02 +0900

    12

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 module Bookbox
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     
