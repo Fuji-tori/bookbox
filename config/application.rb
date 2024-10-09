7d9bb2c49229584b804ce0a4699adbddb9c6318a
Fuji-tori <out.fuji96aya@gmail.com>
2024-10-09 23:40:08 +0900

    6

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module Bookbox
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
