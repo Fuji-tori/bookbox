ef3088a407cc23e68a0fdc083b2b2797dea71a39
Fuji-tori <out.fuji96aya@gmail.com>
2024-10-20 23:52:56 +0900

    delete

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,5 +10,4 @@ class Book < ApplicationRecord
   # validates :book_name, :author, :publication, presence: true, length: { maximum: 100 }
   # validates :synopsis, :review, presence: true,length: { maximum: 1000 }
   # validates :price, presence: true,numericality: { only_integer: true,greater_than: 0, less_than: 10000000}
-  
 end
