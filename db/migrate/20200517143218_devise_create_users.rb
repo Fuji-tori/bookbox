d850d508ce2d74fac0f72a46fd44fb6d137fb5ab
Fuji-tori <out.fuji96aya@gmail.com>
2025-12-06 23:54:55 +0900

    3

--- db/migrate/20200517143218_devise_create_users.rb
+++ db/migrate/20200517143218_devise_create_users.rb
@@ -20,7 +20,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
-      
       ## Confirmable
       # t.string   :confirmation_token
       # t.datetime :confirmed_at
